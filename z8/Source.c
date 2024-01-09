1720cdd1d9290ba9d0d5a91cc702ea30a0de1a33
amatic04 <147614794+amatic04@users.noreply.github.com>
2024-01-09 23:00:30 +0100

    Update Source.c

--- z8/Source.c
+++ z8/Source.c
@@ -2,7 +2,6 @@
 omogućiti unošenje novog elementa u stablo, ispis elemenata (inorder, preorder, postorder i
 level order), brisanje i pronalaženje nekog elementa.*/
 
-//1. dio
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
@@ -18,9 +17,11 @@ typedef struct _node {
 
 position createNode(int);
 position insert(position, int);
+position delete(position, int);
 int preorder(positon);
 int postorder(positon);
 int inorder(positon);
+int levelorder(positon);
 position search(position);
 
 int main() {
@@ -31,10 +32,10 @@ int main() {
 	scanf("%d", &rootValue);
 
 	root = createNode(rootValue);
-	printf("\n1-insert\n2-inorder\n3-preorder\n4-postorder\n5-search\nx-exit");
+	printf("\n1-insert\n2-inorder\n3-preorder\n4-postorder\n5-search\n6-delete\n7-levelorder\nx-exit");
 
 	while (1) {
-		int Value=0;
+		int Value = 0;
 		position result = NULL;
 		printf("\nEnter your choice: ");
 		scanf(" %c", &c);
@@ -66,13 +67,22 @@ int main() {
 		case '5':
 			printf("Enter a value you are looking for: ");
 			scanf("%d", &Value);
-			result=search(root,Value);
-			if (result==NULL)
+			result = search(root, Value);
+			if (result == NULL)
 				printf("Value %d is not found!", Value);
 			else
 				printf("Value %d is found!", Value);
 			continue;
-			
+		case'6':
+			printf("Enter a value you want to delete: ");
+			scanf("%d", &Value);
+			delete(root, Value);
+			printf("\n%d is deleted", Value);
+			continue;
+		case '7':
+			printf("levelorder: ");
+			levelorder(root);
+			continue;
 
 		case'x':
 			break;
@@ -89,7 +99,6 @@ position createNode(int Value) {
 	newNode = (position)malloc(sizeof(node));
 	if (newNode == NULL) {
 		printf("Allocation failed!");
-		exit(-1);
 	}
 	newNode->value = Value;
 	newNode->right = NULL;
@@ -132,13 +141,62 @@ int postorder(position root) {
 	}
 	return 0;
 }
+int levelorder(position root) {
+	if (root) {
+		position queue[100] = { 0 };
+		int front = 0, rear = 0;
+
+		queue[rear++] = root;
+		while (front < rear) {
+			position current = queue[front++];
+			printf("%d", current->value);
+
+			if (current->left != NULL)
+				queue[rear++] = current->left;
+
+			if (current->right != NULL)
+				queue[rear++] = current->right;
+		}
+	}
+	return 0;
+}
 position search(position root, int value) {
-	
-	if (root==NULL || root->value == value) {
+
+	if (root == NULL || root->value == value) {
 		return root;
 	}
 	else if (value < root->value)
 		return search(root->left, value);
 	else
 		return search(root->right, value);
+}
+position delete(position root, int value) {
+	if (root == NULL) {
+		return root;
+	}
+	if (root->value < value)
+		root->right = delete(root->right, value);
+	else if (root->value > value)
+		root->left = delete (root->left, value);
+	else {
+		if (root->left == NULL) {
+			position temp = root->right;
+			free(root);
+			return temp;
+
+		}
+		else if (root->right == NULL) {
+			position temp = root->left;
+			free(root);
+			return temp;
+
+		}
+		position temp = root->left;
+		while (temp->right != NULL) {
+			temp = temp->right;
+		}
+		root->value = temp->value;
+		root->left = delete(root->left, temp->value);
+	}
+	return root;
 }
